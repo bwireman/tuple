80607988e9e0ccce5fee5b3377c41f764b31cfb4
bwireman <bwireman1@gmail.com>
2019-12-07 21:09:08 -0500

    basic container wrapper class

---
+++ containers/container_wrapper.py
@@ -0,0 +1,32 @@
+from threading import Thread
+import time
+
+
+class Container_wrapper:
+    def __init__(self, image, tag):
+        self.name = image
+        self.tag = tag
+        self.container = None
+        self.running = False
+
+    def heartbeat(self, func=None):
+        while self.running:
+            if func:
+                Thread(target=func).start()
+            self.running = self.get_status() == "running"
+            time.sleep(30)
+
+    def run(self, client, func=None):
+        if not self.running:
+            self.client = client
+            self.container = self.client.containers.run(self.name, detach=True)
+            self.running = True
+            Thread(target=self.heartbeat, args=[func]).start()
+        else:
+            # TODO: throw exception if all ready running
+            pass
+
+    def get_status(self, reload=True):
+        if reload:
+            self.container.reload()
+        return self.container.status
