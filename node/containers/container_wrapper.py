a3d9b878148d23a472a43eaf6ab9bd55b997778f
bwireman <bwireman1@gmail.com>
2019-12-26 13:54:36 -0500

    make heartbeat rythm customizeable

--- node/containers/container_wrapper.py
+++ node/containers/container_wrapper.py
@@ -12,19 +12,19 @@ class container_wrapper:
         self.func = None
         self.funcThread = None
         self.heartbeatThread = None
+        self.heartbeatRhythm = None
         self.killed = False
 
-    def heartbeat(self, func=None):
+    def heartbeat(self, func=None, rhythm=30):
         while self.running:
             if func:
-                self.func = func
                 self.funcThread = Thread(target=func)
                 self.funcThread.start()
             self.running = self.get_status() == "running"
-            time.sleep(30)
+            time.sleep(rhythm)
 
         if self.restart and not self.killed:
-            self.run(self.client, self.func)
+            self.run(self.client, self.func, self.heartbeatRhythm)
 
     def kill(self):
         self.running = False
@@ -34,12 +34,14 @@ class container_wrapper:
         self.heartbeatThread.join()
         self.container.stop()
 
-    def run(self, client, func=None):
+    def run(self, client, func=None, heartbeatRhythm=30):
         if not self.running:
+            self.func = func
+            self.heartbeatRhythm = heartbeatRhythm
             self.client = client
             self.container = self.client.containers.run(self.image, detach=True)
             self.running = True
-            self.heartbeatThread = Thread(target=self.heartbeat, args=[func])
+            self.heartbeatThread = Thread(target=self.heartbeat, args=[func, heartbeatRhythm])
             self.heartbeatThread.start()
         else:
             # TODO: throw exception if all ready running
