0b3797506a2bed6b21b9e63f9e2e220559bba504
Benjamin Wireman <ben@censys.io>
2019-12-26 14:13:12 -0500

    Add basic tests, update flake8 and workflows, add kill and rhythm to cw (#2)
    * add testing to make file
    
    * add kill to cw
    
    * add container wrapper basic tests
    
    * make heartbeat rhythm customizable
    
    * flake8 settings and use heartbeat in test
    
    * build test image
    
    * image is in cw test
    
    https://github.com/bwireman/tuple/pull/2

--- node/containers/container_wrapper.py
+++ node/containers/container_wrapper.py
@@ -3,31 +3,46 @@ import time
 
 
 class container_wrapper:
-    def __init__(self, image, tag, name=None, restart=True):
+    def __init__(self, image, tag="latest", name=None, restart=True):
         self.image = image
         self.tag = tag
         self.container = None
         self.running = False
         self.restart = restart
         self.func = None
+        self.funcThread = None
+        self.heartbeatThread = None
+        self.heartbeatRhythm = None
+        self.killed = False
 
-    def heartbeat(self, func=None):
+    def heartbeat(self, func=None, rhythm=30):
         while self.running:
             if func:
-                self.func = func
-                Thread(target=func).start()
+                self.funcThread = Thread(target=func)
+                self.funcThread.start()
             self.running = self.get_status() == "running"
-            time.sleep(30)
+            time.sleep(rhythm)
 
-        if self.restart:
-            self.run(self.client, self.func)
+        if self.restart and not self.killed:
+            self.run(self.client, self.func, self.heartbeatRhythm)
 
-    def run(self, client, func=None):
+    def kill(self):
+        self.running = False
+        self.killed = True
+        if self.funcThread:
+            self.funcThread.join()
+        self.heartbeatThread.join()
+        self.container.stop()
+
+    def run(self, client, func=None, heartbeatRhythm=30):
         if not self.running:
+            self.func = func
+            self.heartbeatRhythm = heartbeatRhythm
             self.client = client
             self.container = self.client.containers.run(self.image, detach=True)
             self.running = True
-            Thread(target=self.heartbeat, args=[func]).start()
+            self.heartbeatThread = Thread(target=self.heartbeat, args=[func, heartbeatRhythm])
+            self.heartbeatThread.start()
         else:
             # TODO: throw exception if all ready running
             pass
