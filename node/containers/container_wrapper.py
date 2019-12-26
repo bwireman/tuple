d5d89d4a3991da2549f95d7fe0abb3c383ca0578
bwireman <bwireman1@gmail.com>
2019-12-26 12:39:22 -0500

    add restart functionality to container wrapper

--- node/containers/container_wrapper.py
+++ node/containers/container_wrapper.py
@@ -3,23 +3,29 @@ import time
 
 
 class container_wrapper:
-    def __init__(self, image, tag):
-        self.name = image
+    def __init__(self, image, tag, name=None, restart=True):
+        self.image = image
         self.tag = tag
         self.container = None
         self.running = False
+        self.restart = restart
+        self.func = None
 
     def heartbeat(self, func=None):
         while self.running:
             if func:
+                self.func = func
                 Thread(target=func).start()
             self.running = self.get_status() == "running"
             time.sleep(30)
 
+        if self.restart:
+            self.run(self.client, self.func)
+
     def run(self, client, func=None):
         if not self.running:
             self.client = client
-            self.container = self.client.containers.run(self.name, detach=True)
+            self.container = self.client.containers.run(self.image, detach=True)
             self.running = True
             Thread(target=self.heartbeat, args=[func]).start()
         else:
